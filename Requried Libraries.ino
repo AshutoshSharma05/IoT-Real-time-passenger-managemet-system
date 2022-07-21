404174d1cc2e47b63d4c9daa191bc2a34cf77104
Ashutosh Sharma <47075542+AshutoshSharma05@users.noreply.github.com>
2022-07-21 13:32:43 +0530

    Add files via upload

---
+++ Requried Libraries.ino
@@ -0,0 +1,4 @@
+    Requried Libraries:-
+•	Download OLED-Display Library:https://github.com/adafruit/Adafruit_SSD1306
+•	Download Adafruit GFX Library: https://github.com/adafruit/Adafruit-GFX-Library
+•	Download Ubidots HTTP Library: https://github.com/ubidots/ubidots-http-esp
